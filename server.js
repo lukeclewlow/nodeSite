2f021b79c993858e0dcbffa71c39702bf1333f1c
Luke Clewlow <luke.clewlow@gmail.com>
2015-03-17 18:06:26 +0000

    playing around with bootstrap, going to start implementing a grid system

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var server = http.createServer(app);
 var port = 9999;
 
 app.set('view engine', 'ejs');
-app.set(express.static(__dirname + '/public'));
+app.set(express.static(__dirname + '/app'));
+app.use("/css", express.static(__dirname + '/app/css'));
+app.use("/js", express.static(__dirname + '/app/js'));
+app.use("/img", express.static(__dirname + '/app/img'));
+
 
 app.get('/', function(request, response){
 	response.render('index', request.query);
